ff89f0aa39f0cd4c7a21b10a0f312243dae5fcea
briancherus <brian.cherus@student.moringaschool.comssh-keygen -t rsa -b 4096 -C brian.cherus@student.moringaschool.com>
2024-01-11 20:40:16 +0300

    completed assignment

--- lib/nyc_api.py
+++ lib/nyc_api.py
@@ -2,13 +2,14 @@
 import requests
 import json
 
-class GetPrograms:
+def program_school(self):
+    # we use the JSON library to parse the API response into nicely formatted JSON
+    programs_list = []
+    programs = json.loads(self.get_programs())
+    for program in programs:
+     programs_list.append(program["agency"])
 
-  def get_programs(self):
-    URL = "http://data.cityofnewyork.us/resource/uvks-tn5n.json"
-
-    response = requests.get(URL)
-    return response.content
+    return programs_list
 
 programs = GetPrograms().get_programs()
 print(programs)
